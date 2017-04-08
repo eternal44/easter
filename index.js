093d152b2ab0d66117d53f6439808af22d89d6fe
James Youn <jamesyoun710@gmail.com>
2017-04-07 22:21:45 -0700

    Add mvp

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var XLSX = require('xlsx-style');
-var workbook = XLSX.readFile('csv-dump/Test.xlsx', {cellStyles:true});
+var workbook = XLSX.readFile('./Test.xlsx', {cellStyles:true});
+var libreconv = require('libreconv').convert;
+var path = require('path');
+
+
+
 
 var sheet = workbook.Sheets.Sheet1
 
@@ -43,38 +48,161 @@ var sortedArtistMap = Object.keys(sheet).reduce(function(memo, key) {
 // [ 'Leann Yan', 'Stacey Test', 'Mary Jane'  ]
 // console.info(Object.keys(sortedArtistMap['Stacey Test']).length) //63 - includes header
 // console.info(Object.keys(sortedArtistMap['Mary Jane']).length) //63 - includes header
-// console.info(Object.keys(sortedArtistMap['Leann Yan']).length) //63 - includes header
+// console.info(Object.keys(sortedArtistMap['Leann Yan'])) //63 - includes header
+// console.log(sortedArtistMap['Leann Yan'].I9)
 // console.log(sortedArtistMap) //63 - includes header
 
 
-generateExcelFile(sortedArtistMap)
-function generateExcelFile(sortedArtistMap) {
+function convertFileToPDF(filePath, outputFormat, opts = {}) {
+  var opts = {
+    output: './convertedFiles/',
+    format: 'pdf'
+  }
 
+  libreconv(path.join(__dirname, filePath), outputFormat, opts)
+}
+
+function main() {
+  var completedFiles = generateExcelFile(sortedArtistMap)
+
+  completedFiles.forEach(function(file){
+    convertFileToPDF(file, 'pdf')
+  })
+}
+
+main()
+
+
+function generateExcelFile(sortedArtistMap) {
+  var completedFiles = []
   for (var artist in sortedArtistMap ) {
     // TODO: dynamically define
-      var range = "A1:I18";
+    var range = "A1:I136";
 
-    // var data = appendObject(sortedArtistMap[artist], range)
-    sortedArtistMap[artist]['!ref'] = range
+    var workBookBody = sortedArtistMap[artist]
+
+    var artistCommissionTotal = calculateArtistCommissionTotal(workBookBody)
+    // if(artist == 'Leann Yan') console.log(workBookBody)
+    var lastRowNumber = findLastRowNumberOnColumn(workBookBody, 'H')
+
+    var commissionTotalCellLocation = 'H' + (lastRowNumber + 1)
+    var totalTitleCellLocation = 'A' + (lastRowNumber + 1)
+
+    workBookBody[commissionTotalCellLocation] = generateCellMetaData(artistCommissionTotal)
+    workBookBody[totalTitleCellLocation] = generateCellMetaData(artist + ' Total')
+
+    workBookBody['!ref'] = range
+    // workBookBody['!cols'] = sheet['!cols']
+
+    workBookBody['!printHeader'] = [1,1]
+
+
+
+
+
+    // ###########
+    // # OPTIONS #
+    // ###########
+
+    workBookBody['!pageSetup'] = {orientation: 'landscape'}
+
+    var wchColumnWidths = [
+      {wch: 5},
+      {wch: 5},
+      {wch: 15},
+      {wch: 7},
+      {wch: 10},
+      {wch: 30},
+      {wch: 5},
+      {wch: 10},
+      {wch: 5}
+    ]
+    workBookBody['!cols'] = wchColumnWidths
 
     var workbook = {
-
       "SheetNames": [
         "Main"
-
       ],
       "Sheets": {
-        'Main': sortedArtistMap[artist]
+        'Main': workBookBody
       }
     }
-    // if(artist == 'Stacey Test') console.log(workbook['Sheets']['Main'])
 
-      XLSX.writeFile(workbook, artist + '.xlsx');
+    var fileName = artist + '.xlsx'
+    XLSX.writeFile(workbook, fileName);
+    completedFiles.push(fileName)
   }
+
+  return completedFiles
+}
+
+function generateCellMetaData(cellValue) {
+  var cellMetaTypes = {
+    number: {
+      t: 'n',
+      v: cellValue,
+      s: {
+        numFmt: '_-* #,##0.00_-;\\-* #,##0.00_-;_-* "-"??_-;_-@_-',
+        font: {
+          bold: true,
+          sz: '10',
+          color: { theme: '1', rgb: 'FFFFFF'  },
+          name: 'Calibri' },
+        border: {}
+      },
+      w: ' 10.50 '
+    },
+    string: {
+      t: 's',
+      v: cellValue,
+      r: "<t>"+ cellValue + "</t>",
+      h: "''" + cellValue + "''",
+      s: {
+        numFmt: 'General',
+        font: {
+          bold: true,
+          sz: '10',
+          color: { theme: '1', rgb: 'FFFFFF'  },
+          name: 'Calibri'
+        },
+        border: {}
+      },
+      w: 'Online'
+    }
+  }
+
+  var type = typeof cellValue
+
+  return cellMetaTypes[type]
+}
+
+
+function findLastRowNumberOnColumn(workBookBody, column) {
+  var result =  Object.keys(workBookBody).reduce(function(lastRowNumber, cell) {
+    if(cell[0] == column){
+      var currentRowNumber = parseInt(cell.slice(1))
+      lastRowNumber = (currentRowNumber > lastRowNumber) ? currentRowNumber : lastRowNumber
+    }
+    return lastRowNumber
+  }, 0)
+  console.log(result)
+
+  return parseInt(result)
 }
 
 function checkKeys(obj) {
   return Object.keys(obj)
+}
+
+function calculateArtistCommissionTotal(workBook) {
+  return  Object.keys(workBook).reduce(function(memo, cell) {
+    var rowNumber = cell.slice(1)
+
+    if(cell[0] == 'H' && rowNumber != 1)
+      memo += workBook[cell].v
+
+    return memo
+  }, 0)
 }
 
 function appendObject(target, source) {
